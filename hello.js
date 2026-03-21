ecca29b58b9db1bce5697590b06b6fdd632c617b
JDiego-13 <diegozack6@gmail.com>
2026-03-20 19:02:07 -0600

    Revert cambios en hello.js

--- hello.js
+++ hello.js
@@ -2,3 +2,8 @@ console.log('Hello, Git!');
 console.log("Hello, GitHub!");
 console.log("TRYING OUT REVER")
 //Implementacion urgente
+
+//Estoy implementando un nuevo codigo
+//code
+//code
+//code
